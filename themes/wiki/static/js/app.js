d82af5d0156a165bf9b08230596309f9db07eac5
Rui Carmo <rui.carmo@gmail.com>
2015-02-16 21:06:24 +0000

    added plain JS handler

---
+++ themes/wiki/static/js/app.js
@@ -0,0 +1,14 @@
+var source = new EventSource("/events");
+
+source.addEventListener('error', function(e) {
+    if (e.readyState == EventSource.CLOSED) {
+        console.log("Server down")
+    }
+    else if( e.readyState == EventSource.OPEN) {
+        console.log("Connecting...")
+    }
+}, false);
+
+source.addEventListener('indexing', function(e) {
+  console.log(e.data);
+}, false);
--- themes/wiki/views/wiki.tpl
+++ themes/wiki/views/wiki.tpl
@@ -1,2 +1,2 @@
 {{!body}}
-%rebase('layout', base_url=base_url, headers=headers, pagename=pagename, seealso=seealso, scripts=['utils.js'])
+%rebase('layout', base_url=base_url, headers=headers, pagename=pagename, seealso=seealso, scripts=['eventsource.js','utils.js','app.js'])
